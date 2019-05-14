bb8fefce5ed9a5918c32e52bc163dae1246675e9
mathan1995 <mathanganjeya@gmail.com>
2019-05-14 12:32:37 +0530

    Sid Nav added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import SiderDemo from "./components/Dashboard";
+function App() {
+  return (
+    <div className="App">
+      <SiderDemo />
+    </div>
+  );
+}
+
+export default App;
