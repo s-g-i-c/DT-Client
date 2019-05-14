7cbcf0f9d6f4512e04580c810aa7b522b3fc6f9c
mathan1995 <mathanganjeya@gmail.com>
2019-05-14 19:56:13 +0530

    Super admin dash

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 import "antd/dist/antd.css";
 import "./Dashboard.css";
-import { Layout, Menu, Icon } from "antd";
+import { Layout, Menu, Icon, Row, Col, Badge, Empty, Ico } from "antd";
 import logo from "./logo.png";
+import "antd/dist/antd.css";
 import "./Dashboard.css";
 
 const { Header, Sider, Content } = Layout;
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
 
 class SiderDemo extends React.Component {
   state = {
@@ -28,13 +31,13 @@ class SiderDemo extends React.Component {
           onCollapse={this.toggle}
         >
           <div className="logo">
-            <img src={logo} margin="34px 16px" width="55px" />{" "}
-            <a href="/">Defect Tracker</a>
+            <img src={logo} padding="10" width="65px" />{" "}
+            <a href="/">Defect.T</a>
           </div>
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+          <Menu theme="dark " mode="inline" defaultSelectedKeys={["1"]}>
             <Menu.Item key="1">
-              <Icon type="user" />
-              <span>Project</span>
+              <Icon type="dashboard" />
+              <span>Dashboard</span>
             </Menu.Item>
             <Menu.Item key="2">
               <Icon type="video-camera" />
@@ -56,14 +59,94 @@ class SiderDemo extends React.Component {
               <Icon type="plus" />
               <span>Manage</span>
             </Menu.Item>
-            <Menu.Item key="7">
-              <Icon type="right-circle" />
-              <span>Details</span>
-            </Menu.Item>
+            <SubMenu
+              key="sub1"
+              title={
+                <span>
+                  <Icon type="sketch" />
+                  <span>Nav Expand</span>
+                </span>
+              }
+            >
+              <Menu.Item key="1">
+                <Icon type="file-add" />
+                Add
+              </Menu.Item>
+              <Menu.Item key="2">
+                <Icon type="alert" />
+                Manage
+              </Menu.Item>
+              <Menu.Item key="3">
+                <Icon type="export" />
+                Test
+              </Menu.Item>
+              <Menu.Item key="4">
+                <Icon type="tag" />
+                Test
+              </Menu.Item>
+            </SubMenu>
           </Menu>
         </Sider>
-        <Layout>
-          <Header style={{ background: "#fff", padding: 0 }} />
+        <Layout
+          style={{
+            background: "#fff"
+          }}
+        >
+          <Header
+            style={{
+              background: "#fff"
+            }}
+          >
+            <Row gutter={16}>
+              <Col className="gutter-row" span={6}>
+                <div className="gutter-box" />
+              </Col>
+              <Col className="gutter-row" span={6}>
+                <div className="gutter-box" />
+              </Col>
+              <Col className="gutter-row" span={6}>
+                <div className="gutter-box" />
+              </Col>
+              <Col className="gutter-row" span={6}>
+                <Menu
+                  onClick={this.handleClick}
+                  selectedKeys={[this.state.current]}
+                  mode="horizontal"
+                >
+                  <Menu.Item key="setting:11">
+                    <Badge count={0} showZero>
+                      <Icon type="bell" />
+                      <a href="#" className="head-example" />
+                    </Badge>
+                  </Menu.Item>
+                  <SubMenu
+                    title={
+                      <span className="submenu-title-wrapper">
+                        <Icon type="user" />
+                        Mathan
+                      </span>
+                    }
+                  >
+                    <MenuItemGroup>
+                      <Menu.Item key="setting:2">
+                        <Icon type="setting" />
+                        Edit Profile
+                      </Menu.Item>
+                      <Menu.Item key="setting:3">
+                        <Icon type="logout" />
+                        Logout
+                      </Menu.Item>
+                    </MenuItemGroup>
+                  </SubMenu>
+                  <Menu.Item>
+                    <Icon type="sync" spin />
+                    Help
+                  </Menu.Item>
+                </Menu>
+              </Col>
+            </Row>
+          </Header>
+
           <Content
             style={{
               margin: "34px 16px",
@@ -72,7 +155,11 @@ class SiderDemo extends React.Component {
               minHeight: 500
             }}
           >
-            Content
+            <Empty
+              imageStyle={{
+                height: 300
+              }}
+            />
           </Content>
         </Layout>
       </Layout>
